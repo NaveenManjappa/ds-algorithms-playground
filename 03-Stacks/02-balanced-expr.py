579f0b9f0270b71408e86eb3c95e805593609b03
Naveen Manjappa <NManjappa@manh.com>
2024-12-10 10:23:31 +0000

    03-stacks Peek and display functions

--- 03-Stacks/02-balanced-expr.py
+++ 03-Stacks/02-balanced-expr.py
@@ -9,19 +9,19 @@ def is_balanced_expression(expr):
             stack.push(char)
 
         if char == ')' or char == ']' or char == '>':
-            pop_char = stack.pop()
+            top = stack.pop()
             #print(pop_char)
-            if char == ')' and pop_char != '(':
+            if char == ')' and top != '(':
                 return False
-            if char == '>' and pop_char != '<':
+            if char == '>' and top != '<':
                 return False
-            if char == ']' and pop_char != '[':
+            if char == ']' and top != '[':
                 return False
-
 
     return stack.is_empty()
 
 my_expr = "(<a>+(b))"
+
 print(f"Result: {is_balanced_expression(my_expr)}")
 
 
---
+++ 03-Stacks/main.py
@@ -0,0 +1,12 @@
+from stack import Stack
+
+my_stack = Stack()
+
+my_stack.pop()
+my_stack.push(10)
+my_stack.push(20)
+my_stack.push(30)
+my_stack.pop()
+print(my_stack.peek())
+
+my_stack.display()
